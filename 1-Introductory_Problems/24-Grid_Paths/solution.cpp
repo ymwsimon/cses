728b4ff9ca6e1a547d64500714b04a0633b67156
Simon <ymwsimon@gmail.com>
2025-05-24 16:03:41 +0100

    Grid Paths

--- 1-Introductory_Problems/24-Grid_Paths/solution.cpp
+++ 1-Introductory_Problems/24-Grid_Paths/solution.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 
-int		dir[][2] = {{1,0},{-1,0},{0,1},{0,-1}};
 bool	vis[9][9];
 
 int	dfs(std::string &s, int idx, int x, int y)
@@ -18,19 +17,14 @@ int	dfs(std::string &s, int idx, int x, int y)
 		return (0);
 	vis[y][x] = true;
 	res = 0;
-	if (s[idx] != '?')
-	{
-		if (!vis[y + dir[s[idx] - '0'][1]][x + dir[s[idx] - '0'][0]])
-			res = dfs(s, idx + 1, x + dir[s[idx] - '0'][0], y + dir[s[idx] - '0'][1]);
-	}
-	else
-	{
-		for (int i = 0; i < 4; ++i)
-		{
-			if (!vis[y + dir[i][1]][x + dir[i][0]])
-				res += dfs(s, idx + 1, x + dir[i][0], y + dir[i][1]);
-		}
-	}
+	if ((s[idx] == '?' || s[idx] == 'U') && !vis[y - 1][x])
+		res += dfs(s, idx + 1, x, y - 1);
+	if ((s[idx] == '?' || s[idx] == 'D') && !vis[y + 1][x])
+		res += dfs(s, idx + 1, x, y + 1);
+	if ((s[idx] == '?' || s[idx] == 'L') && !vis[y][x - 1])
+		res += dfs(s, idx + 1, x - 1, y);
+	if ((s[idx] == '?' || s[idx] == 'R') && !vis[y][x + 1])
+		res += dfs(s, idx + 1, x + 1, y);
 	vis[y][x] = false;
 	return (res);
 }
@@ -46,17 +40,6 @@ int	main(void)
 		vis[8][i] = true;
 		vis[i][0] = true;
 		vis[i][8] = true;
-	}
-	for (size_t idx = 0; idx < s.length(); ++idx)
-	{
-		if (s[idx] == 'U')
-			s[idx] = '3';
-		else if (s[idx] == 'D')
-			s[idx] = '2';
-		else if (s[idx] == 'L')
-			s[idx] = '1';
-		else if (s[idx] == 'R')
-			s[idx] = '0';
 	}
 	for (int i = 1; i <= 7; ++i)
 		for (int j = 1; j <= 7; ++j)
